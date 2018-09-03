f96384c2c697c2c4eb68773ecf6059f2354570b6
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2018-09-04 04:52:01 +0530

    Updated the SQL script

--- Docs/script.sql
+++ Docs/script.sql
@@ -53,3 +53,32 @@ Create table Events
   Date date not null,
   Time varchar(32) not null
 );
+
+/* Questions */
+Create table Questions
+(
+  Id int primary key auto_increment,
+  Content varchar(512) not null,
+  PostedUserId int references Students (Id),
+  PostedTime datetime default CURRENT_TIMESTAMP
+)
+
+/* Answers */
+Create table Answers
+(
+  Id int primary key auto_increment,
+  Content varchar(512) not null,
+  AnsweredUserId int references Employees (Id),
+  PostedTime datetime default CURRENT_TIMESTAMP,
+  QuestionId int references Questions (Id)
+)
+
+/* Books */
+Create table Books
+(
+  Id int primary key auto_increment,
+  Title varchar(64) not null,
+  Description varchar(255) not null,
+  Author varchar(32) not null,
+  Publisher varchar(32) not null
+)
